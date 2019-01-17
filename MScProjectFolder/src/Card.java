d7d86c4f7add5f208cf051cecd90acda8e5c7dc2
Adrian Borg <borgadrian@yahoo.co.uk>
2019-01-17 17:21:33 +0000

    Card class bug fix

--- MScProjectFolder/src/Card.java
+++ MScProjectFolder/src/Card.java
@@ -2,11 +2,11 @@ import java.util.HashMap;
 
 public class Card {
 	private String cardname;
-	private HashMap<String, Integer> values;
+	private HashMap<String, Integer> values = new HashMap<String, Integer>();
 
 	public Card(String cardname, String[] headers, int[] inputValues) {
 		this.cardname = cardname;
-		for(int i=0;i<headers.length-1;i++) {
+		for(int i=0;i<(headers.length-1);i++) {
 			values.put(headers[i+1],inputValues[i]);
 		}
 	}
